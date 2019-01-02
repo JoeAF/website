9a696b136037ee21205a313744af8b7d60f4ce53
JoeAF <45850695+JoeAF@users.noreply.github.com>
2019-01-02 14:36:21 +0000

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -3,9 +3,10 @@ function myFunction() {
 	alert(SpotifyLink)
 	return SpotifyLink;
 }
-var channelID = "UUC0C-w0YjGpqDXGB8IHb662A";
+var channelID = "UC0C-w0YjGpqDXGB8IHb662A";
 var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
-$.getJSON('https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(reqURL)+channelID, function(data) {
+$.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID,
+function(data) {
    var link = data.items[0].link;
    var id = link.substr(link.indexOf("=")+1);
    $("#youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
