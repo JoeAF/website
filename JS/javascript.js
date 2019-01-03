4830ba2897dd7593131280ccb26479d7bafa3f63
JoeAF <45850695+JoeAF@users.noreply.github.com>
2019-01-03 11:06:36 +0000

    Update javascript.js

--- JS/javascript.js
+++ JS/javascript.js
@@ -8,12 +8,12 @@ var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
 $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID,
 function(data) {
    var i;
-   var resultsintohtml = '';
-   for(i=0;i<9;i++){
+   var videos = '';
+   for(i=0;i<10;i++){
 	   var link = data.items[i].link;
 	   console.log(link);
 	   var id = link.substr(link.indexOf("=")+1);
 	   console.log("https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
-	   resultsintohtml += '<div style="position:relative;"><p><iframe src = "https://youtube.com/embed/'+id+'?controls=0&showinfo=0&rel=0" width="420" height="315" frameborder="0" allow="autoplay" allowfullscreen></iframe></a></p><a  href="'+link+'" target="_blank" style="position:absolute; bottom:0; left:0; display:inline-block;"><img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png" width="420px" height="315px"></a><p><a href = "Videos">More Videos</a></p></div>'
-	   $('#resultsholder').html(resultsintohtml);
+	   videos += '<div style="position:relative;"><p><iframe src = "https://youtube.com/embed/'+id+'?controls=0&showinfo=0&rel=0" width="420" height="315" frameborder="0" allow="autoplay" allowfullscreen></iframe></a></p><a  href="'+link+'" target="_blank" style="position:absolute; bottom:0; left:0; display:inline-block;"><img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png" width="420px" height="315px"></a></div>'
+	   $('#videosholder').html(videos);
    }});
